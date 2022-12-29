564b52326eabe0de4ec51f9189c190fe01018250
Adeeb2022 <adeebsamad01@gmail.com>
2022-12-29 14:21:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = [ 'Guadalupe', 'Ollie', 'Aki' ];
+
+function writeCards(names) {
+    let newArr = [];
+    for (let i =0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr;
+}
+writeCards(names);
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
+countDown(5);
